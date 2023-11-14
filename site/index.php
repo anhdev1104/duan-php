bb834a86691d657f8d5060f52d79f907c3441e51
Anh Bui <anhbhpd08263@fpt.edu.vn>
2023-11-14 10:19:52 +0700

    upload php

---
+++ site/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/base.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/res.css">
+    <link rel="stylesheet" href="./css/responsive.css">
+</head>
+
+<body>
+    <div id="root">
+        <?php
+        include '../system/pdo.php';
+
+        include './layout/home/header.php';
+        include './layout/home/main.php';
+        include './layout/home/footer.php';
+        ?>
+    </div>
+    <script src="./js/app.js"></script>
+</body>
+
+</html>
---
+++ site/layout/home/nav.php
@@ -0,0 +1,305 @@
+<section class="header_top">
+    <div class="header_top-block">
+        <!-- HEADER - FIXED -->
+        <div class="header_top-box header-fixed ">
+            <div class="container header_top-wrap-fixed">
+                <a href="#" class="header_box-item-fixed header_box-img">
+                    <img src="./img/logo.webp" alt="" class="header_logo header_logo-fixed">
+                </a>
+                <div class="header_box-item-fixed">
+                    <ul class="header_menu">
+                        <li class="header_menu-item">
+                            <a href="#" class="header-fixed-link">ƯU ĐÃI</a>
+                            <i class="fa-solid fa-angle-down"></i>
+                            <ul class="header_submenu">
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Dịch vụ dành riêng Thành viên
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Get up to 380k cash back
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Flash deal Online
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Month - End Deal
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        FlashSale
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        November Special
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Your Feedback - Our Motivation
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="../../product.php" class="header-fixed-link">NƯỚC HOA</a>
+                            <i class="fa-solid fa-angle-down"></i>
+                            <ul class="header_submenu">
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Chypre (hương đảo Chypre)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Floral (hương hoa cỏ)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Fresh (hương tươi mát)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Fruity (hương trái cây)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Musk (xạ hương)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Leather (hương da thuộc)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Woody (hương gỗ)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Oriental (hương Phương Đông)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Fougere (hương rêu phong)
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="#" class="header-fixed-link">SẢN PHẨM KHÁC</a>
+                            <i class="fa-solid fa-angle-down"></i>
+                            <ul class="header_submenu">
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Sữa tắm nước hoa
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Nến thơm
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="./introduce.html" class="header-fixed-link">GIỚI THIỆU</a>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="./blog.html" class="header-fixed-link">BLOG</a>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="./contact.html" class="header-fixed-link">LIÊN HỆ</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="header_box-item-fixed">
+                    <div class="header_box-action header_box-action-fixed">
+                        <a href="./login.html" class="header_action-item header_action-user">
+                            <i class="fa-regular fa-user"></i>
+                        </a>
+                        <a href="#" class="header_action-item">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </a>
+                        <a href="#" class="header_action-item">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </a>
+                        <!-- responsive tablet - mobile -->
+                        <div class="navbar">
+                            <div class="navbar_icon navbar-icon">
+                                <i class="fa-solid fa-bars"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <!--  -->
+        <p class="header_top-text">FlashSale độc quyền tại Website 21:00 -09:00</p>
+        <div class="header_top-box">
+            <div class="header_top-wrap container">
+                <div class="header_box-item"></div>
+                <div class="header_box-item header_box-img">
+                    <img src="./img/logo.webp" alt="" class="header_logo">
+                </div>
+                <div class="header_box-item">
+                    <div class="header_box-action">
+                        <a href="./login.html" class="header_action-item header_action-user">
+                            <i class="fa-regular fa-user"></i>
+                        </a>
+                        <a href="#" class="header_action-item">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </a>
+                        <a href="#" class="header_action-item">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </a>
+                        <!-- responsive tablet - mobile -->
+                        <div class="navbar">
+                            <div class="navbar_icon navbar-icon">
+                                <i class="fa-solid fa-bars"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="header_box-item--12">
+                    <ul class="header_menu">
+                        <li class="header_menu-item">
+                            <a href="#">ƯU ĐÃI</a>
+                            <i class="fa-solid fa-angle-down"></i>
+                            <ul class="header_submenu">
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Dịch vụ dành riêng Thành viên
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Get up to 380k cash back
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Flash deal Online
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Month - End Deal
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        FlashSale
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        November Special
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Your Feedback - Our Motivation
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="./product.php">NƯỚC HOA</a>
+                            <i class="fa-solid fa-angle-down"></i>
+                            <ul class="header_submenu">
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Chypre (hương đảo Chypre)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Floral (hương hoa cỏ)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Fresh (hương tươi mát)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Fruity (hương trái cây)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Musk (xạ hương)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Leather (hương da thuộc)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Woody (hương gỗ)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Oriental (hương Phương Đông)
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Fougere (hương rêu phong)
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="#">SẢN PHẨM KHÁC</a>
+                            <i class="fa-solid fa-angle-down"></i>
+                            <ul class="header_submenu">
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Sữa tắm nước hoa
+                                    </a>
+                                </li>
+                                <li class="header_submenu-item">
+                                    <a href="#">
+                                        Nến thơm
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="./introduce.html">GIỚI THIỆU</a>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="./blog.html">BLOG</a>
+                        </li>
+                        <li class="header_menu-item">
+                            <a href="./contact.html">LIÊN HỆ</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</section>
